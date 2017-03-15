f9ceead28e5d9e5fa0a08d7032c671c13503da7f
James Burgess <jamesmillerburgess@gmail.com>
2017-03-15 17:55:59 +0100

    Added classes to all tasks

--- imports/ui/layouts/job.js
+++ imports/ui/layouts/job.js
@@ -31,14 +31,14 @@ Template.job.onCreated(function onRendered() {
 });
 
 Template.job.events({
-  'click #shipping-instructions-task': function handleShippingInstructionsTaskClick() {
+  'click .shipping-instructions-task': function handleShippingInstructionsTaskClick() {
     const alert = $('#mbl-alerts').append(
       `<div class="alert alert-warning alert-dismissible fade show" role="alert" data-dismiss="alert">
       <strong>Booking</strong> must be completed before starting <strong>Master Bill of Lading</strong>!
       </div>`).children().last();
     setTimeout(() => alert.alert('close'), 5000);
   },
-  'click #seaquest-draft-task': function handleSeaquestDraftTaskClick() {
+  'click .seaquest-draft-task': function handleSeaquestDraftTaskClick() {
     const alert = $('#seaquest-alerts').append(
       `<div class="alert alert-warning alert-dismissible fade show" role="alert" data-dismiss="alert">
       <strong>Booking</strong> must be completed before starting <strong>House Bill of Lading</strong>!
