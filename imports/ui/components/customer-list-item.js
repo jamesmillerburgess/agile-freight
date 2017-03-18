cf449b3cebecaa3293f3c73728d57cbbd99b90d0
James Burgess <jamesmillerburgess@gmail.com>
2017-03-18 09:05:31 +0100

    Removed old TODO

--- imports/ui/components/customer-list-item.js
+++ imports/ui/components/customer-list-item.js
@@ -5,10 +5,6 @@ import './customer-list-item.html';
 import './customer-list-item.less';
 
 Template.customerListItem.helpers({
-  // TODO: Replace with FlowRouter.go()??
-  link() {
-    return `./customer/${this.customer.code}`;
-  },
   customer() {
     return this.customer;
   },
