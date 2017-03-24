395d1869afc000b9dc431bfa1c41971754e18b46
JBurgess <jamesmillerburgess@gmail.com>
2017-03-24 14:56:41 +0100

    Added paths to newJob fields

--- imports/ui/object-creation-pages/new-job.js
+++ imports/ui/object-creation-pages/new-job.js
@@ -7,3 +7,13 @@ Template.newJob.onCreated(function onCreated() {
   this.data.productOptions = ['Air', 'Ocean', 'Road', ''];
   this.data.incotermOptions = ['CFR', 'CIF', 'CIP', 'CPT', 'DAF', 'DAP', 'DAT', 'DDP', 'DDU', 'EXW', 'FAS', 'FCA', 'FOB', ''];
 });
+
+Template.newJob.events({
+  'click #create-job': function handleClickCreateJob() {
+    const newJob = {};
+    _.each($('#new-job-modal').find('.value'), (doc) => {
+      newJob[$(doc).attr('path')] = doc.innerText;
+    });
+    console.log(newJob);
+  },
+});
