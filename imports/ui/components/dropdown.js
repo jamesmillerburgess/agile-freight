a7450525ec6165a464b1fedcfcfa5e3893ac5d58
James Burgess <jamesmillerburgess@gmail.com>
2017-03-25 08:26:33 +0100

    Removed some console logging

--- imports/ui/components/dropdown.js
+++ imports/ui/components/dropdown.js
@@ -3,13 +3,11 @@ import { Template } from 'meteor/templating';
 import './dropdown.html';
 
 Template.Dropdown.onRendered(function onRendered() {
-  console.log(this.data.value);
   $(this.find('.value'))[0].innerText = this.data.value;
 });
 
 Template.Dropdown.events({
   'click .dropdown-menu a': function handleClickDropdownMenuA(event) {
-    console.log(event.target.innerText);
     $(event.target).parents('.dropdown').find('.value')[0].innerText = event.target.innerText || '';
   },
 });
