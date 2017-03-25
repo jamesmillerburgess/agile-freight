0112725a8a24aaf93323ab283b30479fcaca21aa
James Burgess <jamesmillerburgess@gmail.com>
2017-03-25 10:52:16 +0100

    Insert new job

--- imports/api/jobs/methods.js
+++ imports/api/jobs/methods.js
@@ -1,15 +1,33 @@
 import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
+import { check, Match } from 'meteor/check';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 import { Jobs } from './jobs.js';
 
+const StakeholderRefSchema = new SimpleSchema({
+  _id: { type: String },
+  name: { type: String },
+  address: { type: String, optional: true },
+});
+
+const JobSchema = new SimpleSchema({
+  jobCode: { type: String, optional: true },
+  shipperId: { type: String, optional: true },
+  consigneeId: { type: String, optional: true },
+  // shipper: { type: StakeholderRefSchema, optional: true },
+  // consignee: { type: StakeholderRefSchema, optional: true },
+});
+
 Meteor.methods({
-
-  'jobs.updateField'(jobId, path = '', field, value) {
-
+  'jobs.new': function jobsNewMethod(options) {
+    check(options, JobSchema);
+    Jobs.insert(options);
+  },
+  'jobs.updateField': function jobsUpdateFieldMethod(jobId, path = '', field, value) {
     // Check the parameters
     check(jobId, String);
     check(field, String);
+    check(value, String);
 
     // Build the query
     const query = { _id: jobId };
@@ -21,7 +39,7 @@ Meteor.methods({
       path += '.' + field;
     }
 
-    let update = { $set: { [path]: value } };
+    const update = { $set: { [path]: value } };
 
     // Update the job
     Jobs.update(query, update);
--- imports/ui/object-view-pages/customer.js
+++ imports/ui/object-view-pages/customer.js
@@ -1,4 +1,7 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+
+import { Customers } from '../../api/customers/customers';
 
 import './customer.html';
 import './customer.less';
@@ -6,5 +9,13 @@ import './customer.less';
 Template.customer.helpers({
   creditPercent() {
     return `${Math.round((this.customer.credit.used / this.customer.credit.total) * 100)}%`;
+  },
+});
+
+Template.customer.events({
+  'click #new-job-button': function handleClickNewJobButton(event) {
+    Meteor.call('jobs.new', {
+      shipperId: Customers.findOne({ name: 'Alstom Power Boilers Limited' }, {})._id,
+    });
   },
 });
