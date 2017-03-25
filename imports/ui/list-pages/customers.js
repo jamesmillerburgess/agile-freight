282a311b3a69d732bdcfac2dd25b78bd2449fcf0
James Burgess <jamesmillerburgess@gmail.com>
2017-03-25 08:36:32 +0100

    Added UIGlobals object

--- imports/ui/list-pages/customers.js
+++ imports/ui/list-pages/customers.js
@@ -1,12 +1,13 @@
 import { Template } from 'meteor/templating';
 
 import { Customers } from '../../api/customers/customers';
+import UIGlobals from '../ui-globals';
 
 import './customers.html';
 import './customers.less';
 
 Template.customers.helpers({
   customers() {
-    return Customers.find({}, { limit: 5 });
+    return Customers.find({}, { limit: UIGlobals().listLimit });
   },
 });
---
+++ imports/ui/ui-globals.js
@@ -0,0 +1,5 @@
+export default function UIGlobals() {
+  return {
+    listLimit: 10,
+  };
+}
