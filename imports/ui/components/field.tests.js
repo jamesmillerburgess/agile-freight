a552adcae08e240601e7f696b7f8bc92b923996c
James Burgess <jamesmillerburgess@gmail.com>
2017-03-26 23:04:38 +0200

    Started testing some testing

---
+++ imports/ui/components/field.tests.js
@@ -0,0 +1,21 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+// import { Foo } from '/collections/foo.js';
+import { chai } from 'meteor/practicalmeteor:chai';
+// Will define Template.foo
+// import './field.html'; // your template
+// Will set up Template.foo.__helpers[' bar']
+
+if (Meteor.isClient) {
+  import './field';
+
+  describe('Field handlers', () => {
+    it('Should test bar', () => {
+      const bar = Template.field.__helpers[' overdueClass'].apply(); // don't forget the space, method is ' bar' not 'bar'
+      console.log(bar);
+    });
+  });
+}
+
