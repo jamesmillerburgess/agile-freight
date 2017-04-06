82fe83029f13f7618eadbb70465b9275c25d24a3
James Burgess <jamesmillerburgess@gmail.com>
2017-04-06 12:25:41 +0200

    Render updates properly

--- imports/ui/list-items/UpdateListItem.jsx
+++ imports/ui/list-items/UpdateListItem.jsx
@@ -6,8 +6,27 @@ export default class UpdateListItem extends React.Component {
   }
 
   render() {
+    const { update } = this.props;
     return (
-      <h1>UPDATE LIST ITEM</h1>
+      <div className="latest-update-list-item">
+        <div className="list-item-profile-pic">
+          <a className="user-id" href="">
+            <img
+              className="profile-pic"
+              src="/lib/jburgess%20profile%20pic.png"
+            />
+          </a>
+        </div>
+        <div className="list-item-content">
+          <a className="user-id" href="">{update.userName}</a>{update.message}
+          {update.type === 'Note' ?
+            <div className="quote">
+              {update.note}
+            </div> :
+            null
+          }
+        </div>
+      </div>
     );
   }
 }
