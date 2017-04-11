823d75b8ed83f19a101fd77a8d525dd1289276a4
JBurgess <jamesmillerburgess@gmail.com>
2017-04-11 16:44:08 +0200

    Chart.js 2

--- imports/ui/pages/CustomerOverview.jsx
+++ imports/ui/pages/CustomerOverview.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavLink, Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
-import { Bar, Line } from 'react-chartjs';
+import { Bar } from 'react-chartjs-2';
 
 class CustomerOverviewInner extends React.Component {
   constructor(props) {
@@ -30,7 +30,7 @@ class CustomerOverviewInner extends React.Component {
         </div>
         <div className="content-body">
           <Route path={`/customer/${customer._id}/overview`} exact>
-            <Redirect to={`/customer/${customer._id}/overview/ltm`} />
+            <Redirect to={`/customer/${customer._id}/overview/ltm`}/>
           </Route>
           <div className="row">
             <div className="col-3 kpi">
@@ -62,70 +62,28 @@ class CustomerOverviewInner extends React.Component {
             <div className="chart-label">
               Cargo Volume
             </div>
-            <Bar
-              data={{
-                labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr'],
-                datasets: [{
-                  label: '# of Votes',
-                  data: [12, 19, 3, 5, 2, 3, 5, 9, 6, 7, 1, 3],
-                  backgroundColor: [
-                    'rgba(255,255,255,1)',
-                  ],
-                  borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                  ],
-                  borderWidth: 1,
-                }],
-              }}
-              options={{ responsive: true }}
-              width="900"
-            />
+            <div className="chart-container">
+              <Bar
+                data={{
+                  labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr'],
+                  datasets: [{
+                    data: [12, 19, 3, 5, 2, 3, 5, 9, 6, 7, 1, 3],
+                    borderWidth: 1,
+                  }],
+                }}
+                height={150}
+                options={{
+                  maintainAspectRatio: false,
+                  legend: { display: false },
+                }}
+              />
+            </div>
           </div>
           <div className="chart">
             <div className="chart-label">
               Credit Usage
             </div>
-            <Line
-              data={{
-                labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr'],
-                datasets: [{
-                  data: [0.2, 0.25, 0.4, 0.2, 0.18, 0.3, 0.36, 0.5, 0.45, 0.61, 0.54, 0.48],
-                  backgroundColor: [
-                    'rgba(255,255,255,1)',
-                  ],
-                  borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                  ],
-                  borderWidth: 1,
-                }, {
-                  data: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.7, 0.7, 0.7],
-                  fillColor: [
-                    'rgba(220,200,200,0.2)',
-                  ],
-                  borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                  ],
-                  borderWidth: 1,
-                }],
-              }}
-              options={{ responsive: true }}
-              width="900"
-            />
+
           </div>
         </div>
       </div>
