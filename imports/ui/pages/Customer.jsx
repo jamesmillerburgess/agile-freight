2ff882110095142fc7597dd2fe29b85a88ce45bf
James Burgess <jamesmillerburgess@gmail.com>
2017-04-14 13:22:49 +0200

    Customer => stateless component

--- imports/ui/pages/Customer.jsx
+++ imports/ui/pages/Customer.jsx
@@ -1,4 +1,3 @@
-import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
@@ -14,110 +13,100 @@ import { Customers } from '../../api/customers/customers';
 import { Quotes } from '../../api/quotes/quotes';
 import { Jobs } from '../../api/jobs/jobs';
 
-
-class Customer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { customer } = this.props;
-    return (
+const Customer = ({ customer }) =>
+  (
+    <div>
       <div>
-        <div>
-          <div className="row">
-            <div className="col-2 sidebar">
-              <div className="sidebar-section-spacer" />
-              <div className="sidebar-section-header">
-                CUSTOMER
-              </div>
-              <NavLink
-                to={`/customer/${customer._id}/overview`}
-                activeClassName="active"
-              >
-                Overview
-              </NavLink>
-              <NavLink
-                to={`/customer/${customer._id}/quotes`}
-                activeClassName="active"
-              >
-                Quotes
-              </NavLink>
-              <NavLink
-                to={`/customer/${customer._id}/shipments`}
-                activeClassName="active"
-              >
-                Shipments
-              </NavLink>
-              <NavLink
-                to={`/customer/${customer._id}/invoices`}
-                activeClassName="active"
-              >
-                Invoices
-              </NavLink>
-              <NavLink
-                to={`/customer/${customer._id}/configuration`}
-                activeClassName="active"
-              >
-                Configuration
-              </NavLink>
-              <div className="sidebar-section-spacer" />
-              <div className="sidebar-section-header">
-                UPDATES
-              </div>
-              {/*<MentionFieldContainer />*/}
+        <div className="row">
+          <div className="col-2 sidebar">
+            <div className="sidebar-section-spacer" />
+            <div className="sidebar-section-header">
+              CUSTOMER
             </div>
-            <div className="col-10 content">
-              <div className="content-header">
-                {customer.name}
-              </div>
-              <Route
-                path={`/customer/${customer._id}/overview`}
-                render={props => <CustomerOverview {...props} customer={customer} />}
-              />
-              <Route
-                path={`/customer/${customer._id}/quotes`}
-                render={props => <CustomerQuotes {...props} customer={customer} />
-                }
-              />
-              <Route
-                path={`/customer/${customer._id}/shipments`}
-                render={props => <CustomerShipments {...props} customer={customer} />}
-              />
-              <Route
-                path={`/customer/${customer._id}/invoices`}
-                render={props => <CustomerInvoices {...props} customer={customer} />}
-              />
-              <Route
-                exact
-                path={`/customer/${customer._id}/configuration`}
-                render={props => <CustomerConfiguration {...props} customer={customer} />}
-              />
+            <NavLink
+              to={`/customer/${customer._id}/overview`}
+              activeClassName="active"
+            >
+              Overview
+            </NavLink>
+            <NavLink
+              to={`/customer/${customer._id}/quotes`}
+              activeClassName="active"
+            >
+              Quotes
+            </NavLink>
+            <NavLink
+              to={`/customer/${customer._id}/shipments`}
+              activeClassName="active"
+            >
+              Shipments
+            </NavLink>
+            <NavLink
+              to={`/customer/${customer._id}/invoices`}
+              activeClassName="active"
+            >
+              Invoices
+            </NavLink>
+            <NavLink
+              to={`/customer/${customer._id}/configuration`}
+              activeClassName="active"
+            >
+              Configuration
+            </NavLink>
+            <div className="sidebar-section-spacer" />
+            <div className="sidebar-section-header">
+              UPDATES
+            </div>
+            {/*<MentionFieldContainer />*/}
+          </div>
+          <div className="col-10 content">
+            <div className="content-header">
+              {customer.name}
+            </div>
+            <Route
+              path={`/customer/${customer._id}/overview`}
+              render={props => <CustomerOverview {...props} customer={customer} />}
+            />
+            <Route
+              path={`/customer/${customer._id}/quotes`}
+              render={props => <CustomerQuotes {...props} customer={customer} />
+              }
+            />
+            <Route
+              path={`/customer/${customer._id}/shipments`}
+              render={props => <CustomerShipments {...props} customer={customer} />}
+            />
+            <Route
+              path={`/customer/${customer._id}/invoices`}
+              render={props => <CustomerInvoices {...props} customer={customer} />}
+            />
+            <Route
+              exact
+              path={`/customer/${customer._id}/configuration`}
+              render={props => <CustomerConfiguration {...props} customer={customer} />}
+            />
 
-            </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
 
 Customer.propTypes = {
-  loading: PropTypes.bool,
   customer: PropTypes.object,
   quotes: PropTypes.array,
   jobs: PropTypes.array,
 };
 
 const CustomerContainer = createContainer((props) => {
-  const branch = Meteor.subscribe('branch.active');
-  const loading = !branch.ready();
   const customerId = props.match.params.id;
+  const customer = Customers.findOne(customerId);
+  const quotes = Quotes.find({ 'customer.id': customerId }).fetch();
+  const jobs = Jobs.find({ $or: [{ shipper: customerId }, { consignee: customerId }] }).fetch();
   return {
-    loading,
-    customer: Customers.findOne(customerId),
-    quotes: Quotes.find({ 'customer.id': customerId }).fetch(),
-    jobs: Jobs.find({ $or: [{ shipper: customerId }, { consignee: customerId }] }).fetch(),
+    customer,
+    quotes,
+    jobs,
   };
 }, Customer);
 
