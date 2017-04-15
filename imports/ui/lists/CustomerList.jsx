f3be1ecc0605fd2c324e5c6a44c9bfd21b82d78b
James Burgess <jamesmillerburgess@gmail.com>
2017-04-15 09:59:31 +0200

    More stateless functions

--- imports/ui/lists/CustomerList.jsx
+++ imports/ui/lists/CustomerList.jsx
@@ -1,4 +1,3 @@
-import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
@@ -7,57 +6,47 @@ import { Customers } from '../../api/customers/customers';
 
 import CustomerListItem from '../list-items/CustomerListItem.jsx';
 
-class CustomerList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { customers } = this.props;
-    return (
-      <div className="customer-list">
-        <div className="panel">
-          <div className="panel-header">
-            <div className="panel-header-inner">
-              <div className="icon-container hidden-md-down">
-                <i className="icon fa fa-fw fa-address-card" />
+const CustomerList = ({ customers }) => (
+  <div className="customer-list">
+    <div className="panel">
+      <div className="panel-header">
+        <div className="panel-header-inner">
+          <div className="icon-container hidden-md-down">
+            <i className="icon fa fa-fw fa-address-card" />
+          </div>
+          <div className="panel-header-content container">
+            <div className="row">
+              <div className="panel-title col-2">
+                Customers
               </div>
-              <div className="panel-header-content container">
-                <div className="row">
-                  <div className="panel-title col-2">
-                    Customers
-                  </div>
-                  <div className="col-5">
-                    <ul className="nav">
-                      <li className="nav-item">
-                        <a className="nav-link active" href="#">Mine</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#">New</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#">Top</a>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="col-5">
-                    <form className="form-inline">
-                      <input className="focis-input mr-sm-2" type="text" placeholder="Filter" />
-                    </form>
-                  </div>
-                </div>
+              <div className="col-5">
+                <ul className="nav">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="#">Mine</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">New</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Top</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-5">
+                <form className="form-inline">
+                  <input className="focis-input mr-sm-2" type="text" placeholder="Filter" />
+                </form>
               </div>
             </div>
           </div>
         </div>
-        {customers.map(customer => <CustomerListItem key={customer._id} customer={customer} />)}
       </div>
-    );
-  }
-}
+    </div>
+    {customers.map(customer => <CustomerListItem key={customer._id} customer={customer} />)}
+  </div>
+);
 
 CustomerList.propTypes = {
-  loading: PropTypes.bool,
   customers: PropTypes.array,
 };
 
