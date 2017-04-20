2d388f0333b008a36de958b8d90eedb4122f5e23
JBurgess <jamesmillerburgess@gmail.com>
2017-04-20 09:18:20 +0200

    Parameter decomposition in FreeTextField

--- imports/ui/fields/FreeTextField.jsx
+++ imports/ui/fields/FreeTextField.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FreeTextField = (props) => {
-
+const FreeTextField = ({
+  value,
+  path,
+  valueUpdateCallback,
+  alignRight,
+  unit,
+}) => {
   const handleDropdownTextInputKeyDown = (event) => {
     if (event.key === 'Enter' || event.key === 'Tab') {
-      const value = event.target.value;
-      const needUpdate = props.value !== value;
+      const newValue = event.target.value;
+      const needUpdate = value !== newValue;
 
       // Update if needed and reset the UI because it displays the value twice otherwise
       if (needUpdate) {
-        props.valueUpdateCallback(props.path, value);
+        valueUpdateCallback(path, newValue);
       }
 
       // Toggle the dropdown
@@ -18,45 +23,44 @@ const FreeTextField = (props) => {
     }
   };
 
-    const { value, unit } = props;
-    return (
-      <div className="dropdown">
+  return (
+    <div className="dropdown">
 
-        {/* Field Button */}
-        <div
-          className="value-container dropdown-button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          onClick={() => $('.dropdown-text-input').select()}
-        >
-          <button className={`value ${props.alignRight ? 'align-right' : 'align-left'}`}>
-            <span>{value ? `${value} ${unit || ''}` : <span>&nbsp;</span>}</span>
-          </button>
-        </div>
+      {/* Field Button */}
+      <div
+        className="value-container dropdown-button"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+        onClick={() => $('.dropdown-text-input').select()}
+      >
+        <button className={`value ${alignRight ? 'align-right' : 'align-left'}`}>
+          <span>{value ? `${value} ${unit || ''}` : <span>&nbsp;</span>}</span>
+        </button>
+      </div>
 
-        {/* Field Menu */}
-        <div
-          className="dropdown-menu"
-          aria-labelledby="dropdownMenuButton"
-        >
-          <div className="input-container">
-            <div className="input-row">
-              <input
-                className={`dropdown-text-input ${props.alignRight ? 'align-right' : 'align-left'}`}
-                type="text"
-                defaultValue={value}
-                onKeyDown={handleDropdownTextInputKeyDown}
-              />
-            </div>
-            { unit ? <div className="input-unit">{unit}</div> : '' }
-
+      {/* Field Menu */}
+      <div
+        className="dropdown-menu"
+        aria-labelledby="dropdownMenuButton"
+      >
+        <div className="input-container">
+          <div className="input-row">
+            <input
+              className={`dropdown-text-input ${alignRight ? 'align-right' : 'align-left'}`}
+              type="text"
+              defaultValue={value}
+              onKeyDown={handleDropdownTextInputKeyDown}
+            />
           </div>
+          { unit ? <div className="input-unit">{unit}</div> : '' }
 
         </div>
 
       </div>
-    );
+
+    </div>
+  );
 };
 
 FreeTextField.propTypes = {
