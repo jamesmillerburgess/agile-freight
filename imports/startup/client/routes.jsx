cb1fa3da070edcf3f05455833f8e0cc4943d760f
James Burgess <jamesmillerburgess@gmail.com>
2017-04-24 07:24:19 +0530

    Some test redux stuff

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import app from '../../state/app';
+
+import { appSearch } from '../../state/actions/actions';
+
 import Main from '../../ui/app-core/Main.jsx';
 
+const store = createStore(
+  app,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+store.dispatch(appSearch('hello2'));
+store.dispatch({ type: 'ADD_PACKAGE_LINE', num: 1 });
+store.dispatch({ type: 'ADD_PACKAGE_LINE', num: 2 });
+
+console.log(store.getState());
+store.dispatch({ type: 'REMOVE_PACKAGE_LINE', index: 1 });
+console.log(store.getState());
+store.dispatch({ type: 'EDIT_PACKAGE_LINE', index: 0, packageLine: { num: 3, packageType: 'Bolts' } });
+console.log(store.getState());
 export const renderRoutes = () => (
-  <Main />
+  <Provider store={store}>
+    <Main />
+  </Provider>
 );
---
+++ imports/state/actions/actions.js
@@ -0,0 +1,6 @@
+export const appSearch = text => (
+  {
+    type: 'APP_SEARCH',
+    text,
+  }
+);
