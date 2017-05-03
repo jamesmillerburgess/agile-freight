467c1b7a0a62c29438ebe8b2c3d8ca6dfd3ba211
James Burgess <jamesmillerburgess@gmail.com>
2017-05-03 21:58:50 +0200

    Added script to convert TRUE/FALSE strings into booleans in the UNLocations collection

---
+++ private/importUNLoc.js
@@ -0,0 +1,22 @@
+var db = connect('localhost:3001/meteor');
+
+var c = db.UNLocations.find();
+
+while (c.hasNext()) {
+  var doc = c.next();
+  db.UNLocations.update(
+    { _id: doc._id },
+    {
+      $set: {
+        isPort: doc.isPort === 'TRUE',
+        isRailTerminal: doc.isRailTerminal === 'TRUE',
+        isRoadTerminal: doc.isRoadTerminal === 'TRUE',
+        isAirport: doc.isAirport === 'TRUE',
+        isPostalExchange: doc.isPostalExchange === 'TRUE',
+        isMultimodal: doc.isMultimodal === 'TRUE',
+        isFixedTransport: doc.isFixedTransport === 'TRUE',
+        isBorderCrossing: doc.isBorderCrossing === 'TRUE',
+      },
+    },
+  );
+}
