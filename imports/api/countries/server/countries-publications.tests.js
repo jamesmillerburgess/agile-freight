91048ec7e88cee6eb110ffb6e265b008e2b63c52
James Burgess <jamesmillerburgess@gmail.com>
2017-05-06 20:29:47 +0200

    Countries publication and tests

---
+++ imports/api/countries/server/countries-publications.tests.js
@@ -0,0 +1,27 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+import { chai } from 'meteor/practicalmeteor:chai';
+import { Meteor } from 'meteor/meteor';
+
+import './countries-publications';
+
+chai.should();
+
+if (Meteor.isServer) {
+  describe('Countries Publications', () => {
+    describe('Country Publication', () => {
+      it('registers as a publication', () => {
+        Meteor.server.publish_handlers.should.have.property('countries');
+      });
+
+      it('returns a cursor on the UNLocations collection', () => {
+        const cursor = Meteor.server.publish_handlers.countries();
+
+        cursor.should.have.property('_cursorDescription');
+        cursor._cursorDescription.should.have.property('collectionName');
+        cursor._cursorDescription.collectionName.should.equal('Countries');
+      });
+    });
+  });
+}
