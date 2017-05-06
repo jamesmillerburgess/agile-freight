77bc7e208b22d98d5a702015ced6590aae42722d
James Burgess <jamesmillerburgess@gmail.com>
2017-05-06 20:25:48 +0200

    Countries collection and tests

---
+++ imports/api/countries/countries-collection.js
@@ -0,0 +1,20 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+export const Countries = new Mongo.Collection('Countries');
+
+export const Schemas = {};
+
+Schemas.Country = new SimpleSchema({
+  countryCode: { type: String, optional: true },
+  countryCode3: { type: String, optional: true },
+  countryName: String,
+  regionCode: { type: Number, optional: true },
+  regionName: { type: String, optional: true },
+  subregionCode: { type: Number, optional: true },
+  subregionName: { type: String, optional: true },
+  intermediateRegionCode: { type: Number, optional: true },
+  intermediateRegionName: { type: String, optional: true },
+});
+
+Countries.attachSchema(Schemas.Country);
---
+++ imports/api/countries/countries-collections.tests.js
@@ -0,0 +1,40 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+import { chai } from 'meteor/practicalmeteor:chai';
+import { Meteor } from 'meteor/meteor';
+import StubCollections from 'meteor/hwillson:stub-collections';
+
+import { Countries, Schemas } from './countries-collection';
+
+chai.should();
+
+if (Meteor.isServer) {
+  describe('Countries Collection', () => {
+    beforeEach(() => {
+      StubCollections.stub(Countries);
+      Countries.attachSchema(Schemas.Country);
+    });
+
+    afterEach(() => {
+      StubCollections.restore();
+    });
+
+    it('should have all mandatory and default properties upon insert', () => {
+      const newCountry = { countryName: 'A' };
+      const newCountryId = Countries.insert(newCountry);
+      const newCountryDoc = Countries.findOne(newCountryId);
+
+      newCountryDoc.should.have.property('countryName');
+      newCountryDoc.countryName.should.equal('A');
+      newCountryDoc.should.not.have.property('countryCode');
+      newCountryDoc.should.not.have.property('countryCode3');
+      newCountryDoc.should.not.have.property('regionCode');
+      newCountryDoc.should.not.have.property('regionName');
+      newCountryDoc.should.not.have.property('subregionCode');
+      newCountryDoc.should.not.have.property('subregionName');
+      newCountryDoc.should.not.have.property('intermediateRegionCode');
+      newCountryDoc.should.not.have.property('intermediateRegionName');
+    });
+  });
+}
