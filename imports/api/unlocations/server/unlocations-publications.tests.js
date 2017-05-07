ebaa345f83d0d4e8102492e8ec9490f04c49753f
James Burgess <jamesmillerburgess@gmail.com>
2017-05-07 12:15:39 +0200

    UNLocations list publication and tests

--- imports/api/unlocations/server/unlocations-publications.tests.js
+++ imports/api/unlocations/server/unlocations-publications.tests.js
@@ -10,6 +10,29 @@ chai.should();
 
 if (Meteor.isServer) {
   describe('UNLocations Publications', () => {
+    describe('List Publication', () => {
+      it('registers as a publication', () => {
+        Meteor.server.publish_handlers.should.have.property('unlocations.list');
+      });
+
+      it('returns a cursor on the UNLocations collection', () => {
+        const cursor = Meteor.server.publish_handlers['unlocations.list']();
+
+        cursor.should.have.property('_cursorDescription');
+        cursor._cursorDescription.should.have.property('collectionName');
+        cursor._cursorDescription.collectionName.should.equal('UNLocations');
+      });
+
+      it('publishes UN Locations in the specified list', () => {
+        const cursor = Meteor.server.publish_handlers['unlocations.list'](['a', 'b']);
+
+        cursor._cursorDescription.selector.should.have.property('_id');
+        cursor._cursorDescription.selector._id.should.have.property('$in');
+        cursor._cursorDescription.selector._id.$in[0].should.equal('a');
+        cursor._cursorDescription.selector._id.$in[1].should.equal('b');
+      });
+    });
+
     describe('Country Publication', () => {
       it('registers as a publication', () => {
         Meteor.server.publish_handlers.should.have.property('unlocations.country');
@@ -30,7 +53,5 @@ if (Meteor.isServer) {
         cursor._cursorDescription.selector.country.should.equal('AA');
       });
     });
-
-    describe('Customer Publication', () => {});
   });
 }
