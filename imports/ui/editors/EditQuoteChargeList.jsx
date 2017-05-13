0d614dcde3c7cfbe188e95b916276b73f31197da
James Burgess <jamesmillerburgess@gmail.com>
2017-05-13 21:45:04 +0200

    Put origin chargeline values into table cells

--- imports/ui/editors/EditQuoteChargeList.jsx
+++ imports/ui/editors/EditQuoteChargeList.jsx
@@ -7,60 +7,14 @@ const EditQuoteChargeList = (props) => {
     <tbody className="striped-data">
       {chargeLines.map(chargeLine => (
         <tr key={chargeLine.id}>
-          <td>PICKUP 20&apos;</td>
-          <td>Pickup Charge</td>
-          <td />
-          <td>1</td>
-          <td>USD 200.00</td>
-          <td>USD 200.00</td>
-          <td>USD 200.00</td>
+          <td>{chargeLine.code}</td>
+          <td>{chargeLine.name}</td>
+          <td>{chargeLine.rate}</td>
+          <td>{chargeLine.units}</td>
+          <td>{chargeLine.unitPrice}</td>
+          <td>{chargeLine.amount}</td>
         </tr>
       ))}
-      <tr>
-        <td>PICKUP 20&apos;</td>
-        <td>Pickup Charge</td>
-        <td />
-        <td>1</td>
-        <td>USD 200.00</td>
-        <td>USD 200.00</td>
-        <td>USD 200.00</td>
-      </tr>
-      <tr>
-        <td>AMS 20&apos;</td>
-        <td>Automated Manifest System</td>
-        <td>30 USD/Container</td>
-        <td>1</td>
-        <td>USD 30.00</td>
-        <td>USD 30.00</td>
-        <td>USD 30.00</td>
-      </tr>
-      <tr>
-        <td>ODOC 20&apos;</td>
-        <td>Documentation Charge at Origin</td>
-        <td>Incl. SEAL, EIR. 150 USD/Container</td>
-        <td>1</td>
-        <td>USD 150.00</td>
-        <td>USD 150.00</td>
-        <td>USD 150.00</td>
-      </tr>
-      <tr>
-        <td>OCCF 20&apos;</td>
-        <td>Export Customs Clearance</td>
-        <td>48 USD/Container</td>
-        <td>1</td>
-        <td>USD 48.00</td>
-        <td>USD 48.00</td>
-        <td>USD 48.00</td>
-      </tr>
-      <tr>
-        <td>OTHC 20&apos;</td>
-        <td>Terminal Handling Charge at Origin</td>
-        <td>Incl. ORC. 20&apos;/186 USD</td>
-        <td>1</td>
-        <td>USD 186.00</td>
-        <td>USD 186.00</td>
-        <td>USD 186.00</td>
-      </tr>
     </tbody>
   );
 };
