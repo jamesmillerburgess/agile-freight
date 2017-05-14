52e60d9839bc205b81d9c729919595b7f96c0d0b
James Burgess <jamesmillerburgess@gmail.com>
2017-05-14 16:14:12 +0200

    Modal with basic content + readOnly mode for quote charges

--- imports/ui/editors/EditQuote.jsx
+++ imports/ui/editors/EditQuote.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import Modal from 'react-modal';
 
 import EditQuoteChargeListConnect from './EditQuoteChargeListConnect';
 
 import { CustomerQuotes } from '../../api/customerQuotes/customerQuotesCollection';
+
 import { currencyFormat } from '../formatters/numberFormatters';
 
 class EditQuote extends React.Component {
   componentWillMount() {
     this.props.onLoad(CustomerQuotes.findOne(this.props.match.params.quoteId));
+    this.state = { modalIsOpen: false };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   render() {
@@ -146,7 +159,7 @@ class EditQuote extends React.Component {
                 <tr>
                   <td colSpan="5" />
                   <td colSpan="2">
-                    <button className="submit" onClick={() => Meteor.call('email.send', { to: 'agilityfreightdemo@gmail.com', from: 'agilityfreightdemo@gmail.com', subject: 'subject', text: 'text' })}>
+                    <button className="submit" onClick={this.openModal}>
                       SAVE AND SUBMIT TO CUSTOMER
                     </button>
                   </td>
@@ -155,6 +168,107 @@ class EditQuote extends React.Component {
             </table>
           </div>
         </div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={() => null}
+          onRequestClose={() => null}
+          contentLabel="Modal"
+        >
+          <h1>Email Customer</h1>
+          To: <input /><br />
+          cc: <input /><br />
+          Subject: <input /><br />
+          Message: <textarea /><br />
+          <button onClick={this.closeModal}>Send Email</button>
+          <div className="edit-group">
+            <div className="edit-group-body">
+              <table className="table table-bordered">
+                <tbody>
+                  <tr className="title-row">
+                    <th colSpan="7" className="title">Origin Charges</th>
+                  </tr>
+                  <tr className="column-title-row">
+                    <th className="amount">Fee Code</th>
+                    <th>Fee Name</th>
+                    <th>Comment</th>
+                    <th className="amount">Units</th>
+                    <th className="amount">Unit Price</th>
+                    <th className="amount">Amount</th>
+                    <th className="amount">Final Amount</th>
+                  </tr>
+                </tbody>
+                <EditQuoteChargeListConnect group="Origin" readOnly />
+                <tbody className="subtotal">
+                  <tr>
+                    <td colSpan="5" />
+                    <td>Subtotal</td>
+                    <td>{currency} {currencyFormat(totalOriginCharges)}</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr className="empty-row" />
+                  <tr className="title-row">
+                    <th colSpan="7" className="title">International Charges</th>
+                  </tr>
+                  <tr className="column-title-row">
+                    <th>Fee Code</th>
+                    <th>Fee Name</th>
+                    <th>Comment</th>
+                    <th>Units</th>
+                    <th>Unit Price</th>
+                    <th>Amount</th>
+                    <th>Final Amount</th>
+                  </tr>
+                </tbody>
+                <EditQuoteChargeListConnect group="International" readOnly />
+                <tbody className="subtotal">
+                  <tr>
+                    <td colSpan="5" />
+                    <td>Subtotal</td>
+                    <td>{currency} {currencyFormat(totalInternationalCharges)}</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr className="empty-row" />
+                  <tr className="title-row">
+                    <th colSpan="7" className="title">Destination Charges</th>
+                  </tr>
+                  <tr className="column-title-row">
+                    <th>Fee Code</th>
+                    <th>Fee Name</th>
+                    <th>Comment</th>
+                    <th>Units</th>
+                    <th>Unit Price</th>
+                    <th>Amount</th>
+                    <th>Final Amount</th>
+                  </tr>
+                </tbody>
+                <EditQuoteChargeListConnect group="Destination" readOnly />
+                <tbody className="subtotal">
+                  <tr>
+                    <td colSpan="5" />
+                    <td>Subtotal</td>
+                    <td>{currency} {currencyFormat(totalDestinationCharges)}</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr className="info-row">
+                    <td colSpan="5">
+                      <textarea />
+                    </td>
+                  </tr>
+                </tbody>
+                <tfoot>
+                  <tr className="summary">
+                    <td colSpan="5">Summary</td>
+                    <td>Total Price</td>
+                    <td>{currency} {currencyFormat(totalCharges)}</td>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+        </Modal>
       </div>
     );
   }
--- imports/ui/editors/EditQuoteChargeList.jsx
+++ imports/ui/editors/EditQuoteChargeList.jsx
@@ -13,63 +13,76 @@ const EditQuoteChargeList = (props) => {
       setChargeLineRate,
       setChargeLineUnits,
       setChargeLineUnitPrice,
+      readOnly,
     } = props;
   return (
     <tbody className="striped-data">
       {chargeLines.map(chargeLine => (
         <tr key={chargeLine.id}>
           <td>
-            <input
-              type="text"
-              placeholder=""
-              value={chargeLine.code || ''}
-              onChange={e => setChargeLineCode(chargeLine.id, e.target.value)}
-            />
+            {
+              readOnly ?
+                chargeLine.code :
+                <input
+                  type="text"
+                  placeholder=""
+                  value={chargeLine.code || ''}
+                  onChange={e => setChargeLineCode(chargeLine.id, e.target.value)}
+                />
+            }
           </td>
           <td>
-            <input
-              type="text"
-              placeholder=""
-              value={chargeLine.name || ''}
-              onChange={e => setChargeLineName(chargeLine.id, e.target.value)}
-            />
+            {
+              readOnly ?
+                chargeLine.name :
+                <input
+                  type="text"
+                  placeholder=""
+                  value={chargeLine.name || ''}
+                  onChange={e => setChargeLineName(chargeLine.id, e.target.value)}
+                />
+            }
           </td>
           <td>
-            <textarea
-              type="text"
-              placeholder=""
-              value={chargeLine.rate || ''}
-              onChange={e => setChargeLineRate(chargeLine.id, e.target.value)}
-            />
+            {
+              readOnly ?
+                chargeLine.rate :
+                <textarea
+                  type="text"
+                  placeholder=""
+                  value={chargeLine.rate || ''}
+                  onChange={e => setChargeLineRate(chargeLine.id, e.target.value)}
+                />
+            }
           </td>
           <td>
-            <input
-              className="input-numeric"
-              type="number"
-              placeholder=""
-              value={chargeLine.units || ''}
-              onChange={e => setChargeLineUnits(chargeLine.id, +e.target.value)}
-            />
+            {
+              readOnly ?
+                chargeLine.units :
+                <input
+                  className="input-numeric"
+                  type="number"
+                  placeholder=""
+                  value={chargeLine.units || ''}
+                  onChange={e => setChargeLineUnits(chargeLine.id, +e.target.value)}
+                />
+            }
           </td>
           <td>
-            <input
-              className="input-numeric"
-              type="number"
-              placeholder=""
-              value={chargeLine.unitPrice || ''}
-              onChange={e => setChargeLineUnitPrice(chargeLine.id, +e.target.value)}
-            />
+            {
+              readOnly ?
+                chargeLine.unitPrice :
+                <input
+                  className="input-numeric"
+                  type="number"
+                  placeholder=""
+                  value={chargeLine.unitPrice || ''}
+                  onChange={e => setChargeLineUnitPrice(chargeLine.id, +e.target.value)}
+                />
+            }
           </td>
           <td>{currencyFormat(chargeLine.amount)}</td>
           <td />
-          <td className="icon-cell">
-            <button
-              className="cargo-row-icon"
-              onClick={() => removeChargeLine(chargeLine.id)}
-            >
-              <span className="fa fa-fw fa-minus-circle" />
-            </button>
-          </td>
         </tr>
       ))}
     </tbody>
