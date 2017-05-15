5db0f04c5bc5e135fd953542ecd2331f16d06045
James Burgess <jamesmillerburgess@gmail.com>
2017-05-15 13:26:26 +0200

    Reduce scope of branch subscription

--- imports/api/branch/server/publications.js
+++ imports/api/branch/server/publications.js
@@ -1,22 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import { Offices } from '../../offices/offices';
 import { Customers } from '../../customers/customers-collection';
-import { Quotes } from '../../quotes/quotes-collection';
-import { Shipments } from '../../shipments/shipments';
-import { Invoices } from '../../invoices/invoices-collection';
-import { Jobs } from '../../jobs/jobs';
 import { Countries } from '../../countries/countries-collection';
 import { CustomerQuotes } from '../../customerQuotes/customerQuotesCollection';
 
 Meteor.publish('branch.active', function publishBranchActive() {
   const cursors = [];
   if (this.userId) {
-    cursors.push(Offices.find());
     cursors.push(Customers.find({}, { limit: 10 }));
-    cursors.push(Quotes.find());
-    cursors.push(Shipments.find());
-    cursors.push(Invoices.find());
-    cursors.push(Jobs.find());
     cursors.push(Meteor.users.find({}, { fields: { profile: 1, emails: 1 } }));
     cursors.push(Countries.find());
     cursors.push(CustomerQuotes.find());
