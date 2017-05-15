48fb9113e5ebbdbf95ad4a6b375a4fcbfc7a4ebc
James Burgess <jamesmillerburgess@gmail.com>
2017-05-15 13:30:50 +0200

    Remove unused publication

--- imports/api/customers/server/publications.js
+++ imports/api/customers/server/publications.js
@@ -2,15 +2,6 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
 import { Customers } from '../customers-collection';
-import { Quotes } from '../../quotes/quotes-collection';
-
-Meteor.publish('customers.single', (_id) => {
-  check(_id, String);
-  const cursors = [];
-  cursors.push(Customers.find(_id));
-  cursors.push(Quotes.find({ customerId: _id }));
-  return cursors;
-});
 
 Meteor.publish('customers.search', function customersSearch(filter) {
   check(filter, String);
