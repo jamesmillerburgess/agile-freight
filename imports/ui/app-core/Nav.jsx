fe4b1368381be5c60b56bd410ac522e30dd158c9
James <jamesmillerburgess@gmail.com>
2017-05-17 12:28:07 +0200

    Formatting of nav bar and html content pre-javascript loading

--- imports/ui/app-core/Nav.jsx
+++ imports/ui/app-core/Nav.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Meteor } from 'meteor/meteor';
-import { Link } from 'react-router-dom';
-import { createContainer } from 'meteor/react-meteor-data';
+import {Meteor} from 'meteor/meteor';
+import {Link} from 'react-router-dom';
+import {createContainer} from 'meteor/react-meteor-data';
 
 const NavInner = ({ user, history }) => {
   const logout = () => Meteor.logout(() => history.push('/sign-in'));
@@ -15,73 +15,73 @@ const NavInner = ({ user, history }) => {
         aria-controls="navbarTogglerDemo01" aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon" />
+        <span className="navbar-toggler-icon"/>
       </button>
       <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <Link to="/" className="navbar-brand">Agility Freight</Link>
+        <div className="navbar-brand">
+          <button className="brand">
+            <span className="brand-highlight">AGILITY</span> FREIGHT
+          </button>
+        </div>
         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
           <li className="nav-item">
-            <Link to="/customers" className="nav-link">
-              Customers <span className="sr-only">(current)</span>
-            </Link>
+            <button className="button customers-button">
+              <span className="label">
+                CUSTOMERS
+              </span>
+            </button>
           </li>
           <li className="nav-item">
-            <Link to="/rates" className="nav-link">Rates</Link>
+            <button className="button rates-button">
+              <span className="label">
+                RATES
+              </span>
+            </button>
+          </li>
+          <li className="nav-item">
+            <button className="button profile-button">
+              <span className="label">
+                PROFILE
+              </span>
+            </button>
           </li>
         </ul>
-        <form className="form-inline">
-          <input className="focis-input mr-sm-2" type="text" placeholder="Search" />
-        </form>
+        <div className="form-inline">
+          <input className="focis-input mr-sm-2 search" type="text" placeholder="Search..."/>
+        </div>
         {user ?
           <form className="form-inline my-2 my-lg-0">
             <div className="dropdown">
-              <a
-                id="navbar-profile-button"
-                className="profile-button btn btn-secondary"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <img className="profile-pic" src="/lib/jburgess%20profile%20pic.png" alt="" />
-                <span className="user-name">{user.profile.name}</span>
-              </a>
+              <button className="sign-in-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div className="label">{user.profile.name.toUpperCase()}</div>
+              </button>
               <div className="dropdown-menu">
                 <div className="a-container">
-                  <Link to="/user-profile" id="edit-profile-link" className="dropdown-item">
-                    <i className="fa fa-fw fa-pencil" />
-                    Edit Profile
-                  </Link>
+                  <button className="dropdown-item">
+                    <span className="label">
+                      <span className="fa fa-fw fa-pencil"/>
+                      EDIT PROFILE
+                    </span>
+                  </button>
                 </div>
                 <div className="a-container">
-                  <a
-                    id="sign-out-link"
-                    className="dropdown-item"
-                    onClick={logout}
-                  >
-                    <i className="fa fa-fw fa-sign-out" />
-                    Sign Out
-                  </a>
+                  <button className="dropdown-item" onClick={logout}>
+                    <span className="label">
+                      <span className="fa fa-fw fa-sign-out"/>
+                      SIGN OUT
+                    </span>
+                  </button>
                 </div>
               </div>
             </div>
-          </form> :
-
+          </form>
+          :
           <form className="form-inline my-2 my-lg-0">
-            <Link
-              to="/sign-in"
-              className="profile-button btn btn-secondary"
-            >
-              <span className="user-name">
-                Sign In
-              </span>
-            </Link>
+            <button className="sign-in-button">
+              <span className="label">SIGN IN</span>
+            </button>
           </form>
         }
-        <div className="alert-button-container">
-          <button className="alert-button btn btn-secondary">
-            <i className="fa fa-fw fa-bell-o alert-icon" />
-          </button>
-        </div>
       </div>
     </nav>
   );
