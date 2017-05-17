ab30c7627c2d53b8304308ec1063fe595443b48c
James <jamesmillerburgess@gmail.com>
2017-05-17 15:21:12 +0200

    Put customer list in a content div and give it a footer

--- imports/ui/lists/CustomerList.jsx
+++ imports/ui/lists/CustomerList.jsx
@@ -9,48 +9,21 @@ import CustomerListItem from '../list-items/CustomerListItem.jsx';
 const CustomerListInner = (props) => {
   const { customers, history } = props;
   return (
-    <div className="customer-list">
-      <div className="panel">
-        <div className="panel-header">
-          <div className="panel-header-inner">
-            <div className="icon-container hidden-md-down">
-              <i className="icon fa fa-fw fa-address-card" />
-            </div>
-            <div className="panel-header-content container">
-              <div className="row">
-                <div className="panel-title col-2">
-                  Customers
-                </div>
-                <div className="col-5">
-                  <ul className="nav">
-                    <li className="nav-item">
-                      <a className="nav-link active" href="#">Mine</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">New</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">Top</a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-5">
-                  <form className="form-inline">
-                    <input className="focis-input mr-sm-2" type="text" placeholder="Filter" />
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="">
+      <div className="content customer-list">
+        {
+          customers.map(customer =>
+            <CustomerListItem key={customer._id} customer={customer} history={history} />)
+        }
       </div>
-      {customers.map(customer => <CustomerListItem key={customer._id} customer={customer} history={history} />)}
+      <div className="content-footer-accent customers-footer-accent" />
     </div>
   );
 }
 
 CustomerListInner.propTypes = {
   customers: PropTypes.array,
+  history: PropTypes.object,
 };
 
 const CustomerList = createContainer(() => {
