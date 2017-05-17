758d23b8b618c86536cc59f1a9cffeaa3292c345
James <jamesmillerburgess@gmail.com>
2017-05-17 14:58:40 +0200

    Format home screen

--- imports/ui/app-core/Main.jsx
+++ imports/ui/app-core/Main.jsx
@@ -1,8 +1,8 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { createContainer } from 'meteor/react-meteor-data';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import {createContainer} from 'meteor/react-meteor-data';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom';
 
 // All the pages to render
 import Nav from '../../ui/app-core/Nav.jsx';
@@ -17,7 +17,7 @@ const MainInner = ({ loading }) => {
     if (Meteor.user() || Meteor.loggingIn()) {
       return React.createElement(component, props);
     }
-    return <Redirect to={{ pathname: '/sign-in' }} />;
+    return <Redirect to={{ pathname: '/sign-in' }}/>;
   };
 
   return (
@@ -28,7 +28,44 @@ const MainInner = ({ loading }) => {
             <Nav {...props} />
             {loading ?
               '' :
-              <div className="container page">
+              <div className="content-container">
+                <Route
+                  path="/"
+                  render={
+                    () => (
+                      <div className="">
+                        <div className="content home">
+                          <div className="splash-image-container" />
+                          <div className="home-kpis-container">
+                            <div className="heading">NEWS</div>
+                            <span className="news-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque elit, scelerisque vulputate dolor id, aliquam venenatis enim.</span>
+                            <div className="heading">TODAY</div>
+                            <div className="horizontal-kpi">
+                              <span className="label">QUOTES</span><span className="value">46</span>
+                            </div>
+                            <div className="horizontal-kpi">
+                              <span className="label">SHIPMENTS</span><span className="value">12</span>
+                            </div>
+                            <div className="horizontal-kpi">
+                              <span className="label">INVOICES</span><span className="value">10</span>
+                            </div>
+                            <div className="heading">MAY</div>
+                            <div className="horizontal-kpi">
+                              <span className="label">NET REVENUE</span><span className="value">87 k</span>
+                            </div>
+                            <div className="horizontal-kpi">
+                              <span className="label">JOBS/FTE</span><span className="value">52</span>
+                            </div>
+                            <div className="horizontal-kpi">
+                              <span className="label">NEW CUSTOMERS</span><span className="value">4</span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="content-footer-accent home-footer-accent" />
+                      </div>
+                    )
+                  }
+                />
                 <Route
                   path="/sign-up"
                   render={routeProps => <SignUp {...routeProps} />}
