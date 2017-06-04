0a2df9962149591fd4bedaa23efb2765fa200b0c
James Burgess <jamesmillerburgess@gmail.com>
2017-06-04 22:26:02 +0200

    Customer action creators

---
+++ imports/state/actions/customerActions.js
@@ -0,0 +1,28 @@
+import { makeActionCreator } from './action-utils';
+import * as ACTION_TYPES from './actionTypes';
+
+// CUSTOMER
+export const loadCustomer            = makeActionCreator(
+  ACTION_TYPES.LOAD_CUSTOMER,
+  'customer',
+);
+export const setCustomerName         = makeActionCreator(
+  ACTION_TYPES.SET_CUSTOMER_NAME,
+  'name',
+);
+export const setCustomerAddress      = makeActionCreator(
+  ACTION_TYPES.SET_CUSTOMER_ADDRESS,
+  'address',
+);
+export const setCustomerEmailAddress = makeActionCreator(
+  ACTION_TYPES.SET_CUSTOMER_EMAIL_ADDRESS,
+  'emailAddress',
+);
+export const setCustomerCurrency     = makeActionCreator(
+  ACTION_TYPES.SET_CUSTOMER_CURRENCY,
+  'currency',
+);
+export const setCustomerBranch       = makeActionCreator(
+  ACTION_TYPES.SET_CUSTOMER_BRANCH,
+  'branch',
+);
