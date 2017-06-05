de8a6b7a43e0cf4c892f93d8129f67da90d86f89
James Burgess <jamesmillerburgess@gmail.com>
2017-06-05 11:49:04 +0200

    Remove quote.newFromRateSearch

--- imports/api/quotes/quoteMethods.js
+++ imports/api/quotes/quoteMethods.js
@@ -40,31 +40,6 @@ Meteor.methods({
 
     return newQuoteId;
   },
-  'quote.newFromRateSearch': function quotesNewFromRateSearch(options) {
-    check(options, Object);
-    check(options.customerId, String);
-    check(options.cargo, Object);
-    check(options.movement, Object);
-    check(options.otherServices, Object);
-
-    // Check if the customerId is valid
-    const customer = Customers.findOne(options.customerId);
-    if (!customer) {
-      throw new Error('Invalid customer ID');
-    }
-
-    // Insert quote
-    const update = {
-      ...options,
-      status: 'Draft',
-      charges: [],
-    };
-    const quoteId = Quotes.insert(update);
-
-    // Update the customer
-    Customers.update({ _id: options.customerId }, { $push: { quotes: quoteId } });
-    return quoteId;
-  },
   'quote.submit': function quoteSubmit(quoteId, email, expiryDate) {
     check(quoteId, String);
     check(email, Object);
