270210337ca7b1ff81e385fea6c75aa69567d200
James Burgess <jamesmillerburgess@gmail.com>
2017-06-18 16:58:40 +0200

    Meteor Method users.unfavoriteCustomer

--- imports/api/users/userMethods.tests.js
+++ imports/api/users/userMethods.tests.js
@@ -12,15 +12,12 @@ chai.should();
 
 if (Meteor.isServer) {
   describe('User Methods', () => {
-    let userId;
-    beforeEach(() => {
-      Meteor.users.remove({});
-      userId = Meteor.users.insert({ profile: {} });
-    });
-
     describe('users.favoriteCustomer', () => {
+      let userId;
       let customerId;
       beforeEach(() => {
+        Meteor.users.remove({});
+        userId = Meteor.users.insert({ profile: {} });
         Customers.remove({});
         customerId = Customers.insert({});
       });
@@ -49,6 +46,44 @@ if (Meteor.isServer) {
       it('throws an error if the customerId is already a favorite', () => {
         Meteor.call('users.favoriteCustomer', userId, customerId);
         (() => Meteor.call('users.favoriteCustomer', userId, customerId))
+          .should
+          .throw(Error);
+      });
+    });
+
+    describe('users.unfavoriteCustomer', () => {
+      let userId;
+      let customerId;
+      beforeEach(() => {
+        Customers.remove({});
+        customerId = Customers.insert({});
+        Meteor.users.remove({});
+        userId =
+          Meteor.users.insert({ profile: { favoriteCustomers: [customerId] } });
+      });
+
+      it('removes the customerId to the favorite customers array', () => {
+        Meteor.call('users.unfavoriteCustomer', userId, customerId);
+        Meteor.users.findOne(userId).profile.favoriteCustomers.length
+              .should.equal(0);
+      });
+
+      it('throws an error if the userId is invalid', function () {
+        this.timeout(10000);
+        (() => Meteor.call('users.favoriteCustomer', 'x', customerId))
+          .should
+          .throw(Error);
+      });
+
+      it('throws an error if the customerId is invalid', () => {
+        (() => Meteor.call('users.favoriteCustomer', userId, 'x'))
+          .should
+          .throw(Error);
+      });
+
+      it('throws an error if the customerId is not already a favorite', () => {
+        Meteor.call('users.unfavoriteCustomer', userId, customerId);
+        (() => Meteor.call('users.unfavoriteCustomer', userId, customerId))
           .should
           .throw(Error);
       });
