eb1c889d2e865aaf55acb4e444d55f084724a1cb
James Burgess <jamesmillerburgess@gmail.com>
2017-06-18 15:58:00 +0200

    users.favoriteCustomer Meteor Method and tests

--- imports/api/users/userMethods.js
+++ imports/api/users/userMethods.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
+import { Customers } from '../customers/customersCollection';
+
 Meteor.methods({
   'users.updateField': function usersUpdateField(userId, path, value) {
     // Check the parameters
@@ -9,7 +11,7 @@ Meteor.methods({
     check(value, String);
 
     // Build the query and update criteria
-    const query = { _id: userId };
+    const query  = { _id: userId };
     const update = { $set: { [path]: value } };
 
     // Update the job
@@ -19,5 +21,29 @@ Meteor.methods({
 
     // Update search
     // updateSearch(jobId);
+  },
+  'users.favoriteCustomer': function usersFavoriteCustomer(userId, customerId) {
+    check(userId, String);
+    check(customerId, String);
+
+    const user = Meteor.users.findOne(userId);
+    if (!user) {
+      throw new Error(`UserId ${userId} does not exist.`);
+    }
+
+    if (!Customers.findOne(customerId)) {
+      throw new Error(`CustomerId ${customerId} does not exist.`);
+    }
+
+    if (
+      user.profile &&
+      user.profile.favoriteCustomers &&
+      user.profile.favoriteCustomers.indexOf(customerId) !== -1
+    ) {
+      throw new Error(`CustomerId ${customerId} is already a favorite`);
+    }
+
+    Meteor.users.update({ _id: userId },
+      { $push: { 'profile.favoriteCustomers': customerId } });
   },
 });
