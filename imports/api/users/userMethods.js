3896528319bd400bb20ff5fe44e5c04c08cbe1af
James Burgess <jamesmillerburgess@gmail.com>
2017-06-18 15:58:34 +0200

    Remove unused Meteor Method users.updateField

--- imports/api/users/userMethods.js
+++ imports/api/users/userMethods.js
@@ -4,24 +4,6 @@ import { check } from 'meteor/check';
 import { Customers } from '../customers/customersCollection';
 
 Meteor.methods({
-  'users.updateField': function usersUpdateField(userId, path, value) {
-    // Check the parameters
-    check(userId, String);
-    check(path, String);
-    check(value, String);
-
-    // Build the query and update criteria
-    const query  = { _id: userId };
-    const update = { $set: { [path]: value } };
-
-    // Update the job
-    Meteor.users.update(query, update);
-
-    return Meteor.users.findOne(query);
-
-    // Update search
-    // updateSearch(jobId);
-  },
   'users.favoriteCustomer': function usersFavoriteCustomer(userId, customerId) {
     check(userId, String);
     check(customerId, String);
