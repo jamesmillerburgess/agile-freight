08fde8a16690a9b366e654990669871ba41f2447
James Burgess <jamesmillerburgess@gmail.com>
2017-06-21 14:14:39 +0200

    Comment out service worker since it is breaking Safari

--- client/main.jsx
+++ client/main.jsx
@@ -4,9 +4,9 @@ import { renderRoutes } from '../imports/startup/client/routes.jsx';
 
 Meteor.startup(() => {
   Meteor.startup(() => {
-    navigator.serviceWorker.register('/sw.js')
-      .then()
-      .catch(err => console.log('ServiceWorker registration failed: ', err));
+    // navigator.serviceWorker.register('/sw.js')
+    //   .then()
+    //   .catch(err => console.log('ServiceWorker registration failed: ', err));
   });
   render(renderRoutes(), document.getElementById('app'));
 });
