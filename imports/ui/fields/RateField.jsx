5bceaff9e7abe44d9c8cd3a5330f8cb48ef2ae92
James Burgess <jamesmillerburgess@gmail.com>
2017-06-23 16:12:00 +0200

    Disable rate applicability levels which have no rates

--- imports/ui/fields/RateField.jsx
+++ imports/ui/fields/RateField.jsx
@@ -19,7 +19,7 @@ const RateField = (props) => {
       rateValue = <div />;
     }
     return (
-      <div className="rate-option">
+      <div className={`rate-option ${option.disabled ? 'disabled' : ''}`}>
         <div className="rate-option-label">{option.label}</div>
         {rateValue}
       </div>
@@ -43,21 +43,25 @@ const RateField = (props) => {
           value: 'supplier',
           label: 'SUPPLIER',
           rate: props.rates.supplier,
+          disabled: typeof props.rates.supplier === 'undefined',
         },
         {
           value: 'location',
           label: 'LOCATION',
           rate: props.rates.location,
+          disabled: typeof props.rates.location === 'undefined',
         },
         {
           value: 'country',
           label: 'COUNTRY',
           rate: props.rates.country,
+          disabled: typeof props.rates.country === 'undefined',
         },
         {
           value: 'global',
           label: 'GLOBAL',
           rate: props.rates.global,
+          disabled: typeof props.rates.global === 'undefined',
         },
       ]}
       clearable={false}
