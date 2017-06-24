c9af7c0aa196d9a8bfda412a7454acadefe7ac61
James Burgess <jamesmillerburgess@gmail.com>
2017-06-24 07:33:19 +0200

    Extract pattern and function from unlocations Meteor Method

--- imports/api/unlocations/unlocationsMethods.js
+++ imports/api/unlocations/unlocationsMethods.js
@@ -6,56 +6,58 @@ import { Countries } from '../countries/countriesCollection';
 
 import { buildSearchRegExp } from '../../ui/searchUtils';
 
-Meteor.methods({
-  'unlocations.search': function unlocationsSearch(options) {
-    check(options, {
-      country: Match.Maybe(String),
-      search: Match.Maybe(String),
-      id: Match.Maybe(String),
-      locations: Boolean,
-      airports: Boolean,
-      seaports: Boolean,
-    });
+const unlocationsSearchPattern = {
+  country: Match.Maybe(String),
+  search: Match.Maybe(String),
+  id: Match.Maybe(String),
+  locations: Boolean,
+  airports: Boolean,
+  seaports: Boolean,
+};
 
-    if (!options.search && options.id) {
-      return UNLocations.find({ _id: options.id }).fetch();
-    }
-    //
-    // if (!Countries.findOne({ countryCode: options.country })) {
-    //   throw new Error(`Invalid country '${options.country}'`);
-    // }
-    const query = {
-      // countryCode: options.country,
-      search: { $regex: buildSearchRegExp(options.search) },
-    };
-    let results = [];
-    if (options.locations) {
-      results = [
-        ...results,
-        ...UNLocations.find(
-          { ...query, isSeaport: false, isAirport: false },
-          { limit: 10 },
-        ).fetch(),
-      ];
-    }
-    if (options.airports) {
-      results = [
-        ...results,
-        ...UNLocations.find(
-          { ...query, isAirport: true },
-          { limit: 10 },
-        ).fetch(),
-      ];
-    }
-    if (options.seaports) {
-      results = [
-        ...results,
-        ...UNLocations.find(
-          { ...query, isSeaport: true },
-          { limit: 10 },
-        ).fetch(),
-      ];
-    }
-    return results;
-  },
-});
+const unlocationsSearch = (options) => {
+  check(options, unlocationsSearchPattern);
+
+  if (!options.search && options.id) {
+    return UNLocations.find({ _id: options.id }).fetch();
+  }
+  //
+  // if (!Countries.findOne({ countryCode: options.country })) {
+  //   throw new Error(`Invalid country '${options.country}'`);
+  // }
+  const query = {
+    // countryCode: options.country,
+    search: { $regex: buildSearchRegExp(options.search) },
+  };
+  let results = [];
+  if (options.locations) {
+    results = [
+      ...results,
+      ...UNLocations.find(
+        { ...query, isSeaport: false, isAirport: false },
+        { limit: 10 },
+      ).fetch(),
+    ];
+  }
+  if (options.airports) {
+    results = [
+      ...results,
+      ...UNLocations.find(
+        { ...query, isAirport: true },
+        { limit: 10 },
+      ).fetch(),
+    ];
+  }
+  if (options.seaports) {
+    results = [
+      ...results,
+      ...UNLocations.find(
+        { ...query, isSeaport: true },
+        { limit: 10 },
+      ).fetch(),
+    ];
+  }
+  return results;
+};
+
+Meteor.methods({ 'unlocations.search': unlocationsSearch });
