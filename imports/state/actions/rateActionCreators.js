f6b9879accf66b090bcef80d5a1089ecde843b46
James Burgess <jamesmillerburgess@gmail.com>
2017-06-28 15:54:50 +0200

    Updated action creators and connect component for editing rates

--- imports/state/actions/rateActionCreators.js
+++ imports/state/actions/rateActionCreators.js
@@ -12,16 +12,35 @@ export const setRateRoute = makeActionCreator(TYPES.SET_RATE_ROUTE, 'route');
 export const toggleRateIsSplitByCargoType = makeActionCreator(
   TYPES.TOGGLE_RATE_IS_SPLIT_BY_CARGO_TYPE,
 );
-export const setRateBasis = makeActionCreator(TYPES.SET_RATE_BASIS, 'basis');
-export const setRateUnitPrice = makeActionCreator(
+export const setRateBasis = makeActionCreator(
+  TYPES.SET_RATE_BASIS, 'basis', 'cargoType',
+);
+export const addRateRange = makeActionCreator(
+  TYPES.ADD_RATE_RANGE,
+  'id',
+  'range',
+  'cargoType',
+);
+export const setRateRangeUnitPrice = makeActionCreator(
   TYPES.SET_RATE_RANGE_UNIT_PRICE,
+  'id',
   'unitPrice',
+);
+export const setRateRangeMaximumUnits = makeActionCreator(
+  TYPES.SET_RATE_RANGE_MAXIMUM_UNITS,
+  'id',
+  'maximumUnits',
+);
+export const removeRateRange = makeActionCreator(
+  TYPES.REMOVE_RATE_RANGE,
+  'id',
 );
 export const setRateCurrency = makeActionCreator(
   TYPES.SET_RATE_CURRENCY,
   'currency',
 );
-export const setRateMinumumAmount = makeActionCreator(
+export const setRateMinimumAmount = makeActionCreator(
   TYPES.SET_RATE_MINIMUM_AMOUNT,
   'minimumAmount',
+  'cargoType',
 );
--- imports/ui/editors/EditRateConnect.jsx
+++ imports/ui/editors/EditRateConnect.jsx
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import { Mongo } from 'meteor/mongo';
 
 import EditRate from './EditRate.jsx';
 import * as actions from '../../state/actions/rateActionCreators';
@@ -30,15 +31,36 @@ const mapDispatchToProps = (dispatch, ownProps) => {
         dispatch(actions.setRateChargeCode(chargeCode)),
       onChangeRateLevel: level => dispatch(actions.setRateLevel(level)),
       onChangeRateRoute: route => dispatch(actions.setRateRoute(route)),
-      onChangeIsSplitByCargoType:
-        () => dispatch(actions.toggleRateIsSplitByCargoType()),
-      onChangeRateBasis: basis => dispatch(actions.setRateBasis(basis)),
-      onChangeRateUnitPrice: unitPrice =>
-        dispatch(actions.setRateUnitPrice(unitPrice)),
+      onChangeIsSplitByCargoType: () => dispatch(actions.toggleRateIsSplitByCargoType()),
+      onChangeAnyBasis: basis =>
+        dispatch(actions.setRateBasis(basis, 'any')),
+      onChangeLooseBasis: basis =>
+        dispatch(actions.setRateBasis(basis, 'loose')),
+      onChangeContainerizedBasis: basis =>
+        dispatch(actions.setRateBasis(basis, 'containerized')),
+      onChangeAnyMinimumAmount: minimumAmount =>
+        dispatch(actions.setRateMinimumAmount(minimumAmount, 'any')),
+      onChangeLooseMinimumAmount: minimumAmount =>
+        dispatch(actions.setRateMinimumAmount(minimumAmount, 'loose')),
+      onChangeContainerizedMinimumAmount: minimumAmount =>
+        dispatch(actions.setRateMinimumAmount(minimumAmount, 'containerized')),
+      onAddAnyRateRange: () =>
+        dispatch(actions.addRateRange(new Mongo.ObjectID()._str, {}, 'any')),
+      onAddLooseRateRange: () =>
+        dispatch(actions.addRateRange(new Mongo.ObjectID()._str, {}, 'loose')),
+      onAddContainerizedRateRange: () =>
+        dispatch(actions.addRateRange(
+          new Mongo.ObjectID()._str,
+          {},
+          'containerized',
+        )),
+      onChangeRateRangeUnitPrice: (id, unitPrice) =>
+        dispatch(actions.setRateRangeUnitPrice(id, unitPrice)),
+      onChangeRateRangeMaximumUnits: (id, maximumUnits) =>
+        dispatch(actions.setRateRangeMaximumUnits(id, maximumUnits)),
+      onRemoveRateRange: id => dispatch(actions.removeRateRange(id)),
       onChangeRateCurrency: currency =>
         dispatch(actions.setRateCurrency(currency)),
-      onChangeRateMinimumAmount: minimumAmount =>
-        dispatch(actions.setRateMinumumAmount(minimumAmount)),
     },
   };
 };
