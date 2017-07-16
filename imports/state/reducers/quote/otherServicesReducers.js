e0aadb67a698e6aa182aa08ad7742ea03fe1453c
James Burgess <jamesmillerburgess@gmail.com>
2017-07-16 15:57:52 +0200

    otherServicesReducers.js => lodash/fp

--- imports/state/reducers/quote/otherServicesReducers.js
+++ imports/state/reducers/quote/otherServicesReducers.js
@@ -1,19 +1,29 @@
-import { setProp } from '../reducer-utils';
+import { set } from 'lodash/fp';
 import * as ACTION_TYPES from '../../actions/actionTypes';
 
 const otherServicesDefaultState = { insurance: false, customsClearance: false };
 
-export const otherServices = (state  = otherServicesDefaultState,
-                              action = { type: '' }) => {
+export const otherServices = (
+  state = otherServicesDefaultState,
+  action = { type: '' },
+) => {
   switch (action.type) {
     case ACTION_TYPES.LOAD_QUOTE:
       return action.quote.otherServices || otherServicesDefaultState;
     case ACTION_TYPES.TOGGLE_INSURANCE:
-      return setProp(state, 'insurance', !state.insurance);
+      return set('insurance', !state.insurance, state);
     case ACTION_TYPES.TOGGLE_EXPORT_CUSTOMS_CLEARANCE:
-      return setProp(state, 'exportCustomsClearance', !state.exportCustomsClearance);
+      return set(
+        'exportCustomsClearance',
+        !state.exportCustomsClearance,
+        state,
+      );
     case ACTION_TYPES.TOGGLE_IMPORT_CUSTOMS_CLEARANCE:
-      return setProp(state, 'importCustomsClearance', !state.importCustomsClearance);
+      return set(
+        'importCustomsClearance',
+        !state.importCustomsClearance,
+        state,
+      );
     default:
       return state;
   }
