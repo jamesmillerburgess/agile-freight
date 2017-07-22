7967aad6c2f6cd4e6c4ca510d268c61fcbdd78f3
James Burgess <jamesmillerburgess@gmail.com>
2017-07-22 15:08:27 +0200

    Shipment method 'shipment.new'

---
+++ imports/api/shipments/shipmentMethods.js
@@ -0,0 +1,29 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
+import { Shipments } from './shipmentsCollection';
+import { Quotes } from '../quotes/quotesCollection';
+import { Customers } from '../customers/customersCollection';
+
+Meteor.methods({
+  'shipment.new': function shipmentNew(quoteId) {
+    check(quoteId, String);
+
+    const quote = Quotes.findOne(quoteId);
+    if (!quote || !quote.customerId) {
+      throw new Error('Invalid quote id');
+    }
+    const { customerId } = quote;
+    const shipmentId = Shipments.insert({
+      customerId,
+      quoteId,                    
+      createdOn: new Date(),
+      status: 'Draft',
+    });
+
+    Quotes.update({ _id: quoteId }, { $push: { shipments: shipmentId } });
+    Customers.update({ _id: customerId }, { $push: { shipments: shipmentId } });
+
+    return shipmentId;
+  },
+});
---
+++ imports/api/shipments/shipmentMethods.tests.js
@@ -0,0 +1,58 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+import { chai } from 'meteor/practicalmeteor:chai';
+import { Meteor } from 'meteor/meteor';
+
+import { Shipments } from './shipmentsCollection';
+import { Quotes } from '../quotes/quotesCollection';
+import { Customers } from '../customers/customersCollection';
+
+import './shipmentMethods';
+
+chai.should();
+
+if (Meteor.isServer) {
+  describe('Shipment Methods', () => {
+    beforeEach(() => {
+      Shipments.remove({});
+      Quotes.remove({});
+      Customers.remove({});
+      Quotes.insert({ _id: 'a', customerId: 'b' });
+      Customers.insert({ _id: 'b', quotes: 'a' });
+    });
+    describe('shipment.new', () => {
+      it('inserts a shipment into the collection', () => {
+        Shipments.find({}).count().should.equal(0);
+        Meteor.call('shipment.new', 'a');
+
+        Shipments.find({}).count().should.equal(1);
+      });
+
+      it('returns the id of the new shipment', () => {
+        const shipmentId = Meteor.call('shipment.new', 'a');
+        Shipments.findOne()._id.should.equal(shipmentId);
+      });
+
+      it('inserts a shipment with the given quoteId', () => {
+        const shipmentId = Meteor.call('shipment.new', 'a');
+        Shipments.findOne(shipmentId).quoteId.should.equal('a');
+      });
+
+      it('updates the given customer with the new shipment id', () => {
+        const shipmentId = Meteor.call('shipment.new', 'a');
+        Customers.findOne('b').shipments[0].should.equal(shipmentId);
+      });
+
+      it('sets the created on date', () => {
+        const shipmentId = Meteor.call('shipment.new', 'a');
+        Shipments.findOne(shipmentId).createdOn.should.be.instanceof(Date);
+      });
+
+      it('sets the status to \'Draft\'', () => {
+        const shipmentId = Meteor.call('shipment.new', 'a');
+        Shipments.findOne(shipmentId).status.should.equal('Draft');
+      });
+    });
+  });
+}
