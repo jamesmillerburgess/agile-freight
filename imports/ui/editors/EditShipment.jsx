40ef6f8de3a18c19d94cb4e7cd51232a0f7503b9
James Burgess <jamesmillerburgess@gmail.com>
2017-09-03 20:53:15 +0200

    Prepare routing and editor header for operations/accounting split

--- imports/ui/editors/EditShipment.jsx
+++ imports/ui/editors/EditShipment.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
+import { Route, Link } from 'react-router-dom';
 
 import EditCargo from './EditCargo.jsx';
 import EditMovement from './EditMovement.jsx';
@@ -51,164 +52,194 @@ export const AirWaybillButton = ({ shipment }) =>
     AIR WAYBILL
   </button>;
 
-const EditShipment = ({ shipment, dispatchers, history, match }) =>
-  <div className="new-quote">
-    <div className="process-header">
-      <div className="title">
-        SHIPMENT {shipment.reference}
+export const EditShipmentOperations = ({ shipment, dispatchers, match }) =>
+  <div className="panel container form-section">
+    <div className="title">
+      <div className="cargo-row-icon" />
+      PARTIES
+    </div>
+    <div className="pickup-delivery-wrapper">
+      <div className="cargo-row-icon" />
+      <div className="field select-country">
+        <div className="label">SHIPPER</div>
+        <input
+          value={shipment.shipper || ''}
+          onChange={e => dispatchers.onChangeShipper(e.target.value)}
+        />
       </div>
-      <div className="breadcrumbs">
-        <div className="breadcrumb active customer">
-          {shipment.status ? shipment.status.toUpperCase() : ''}
-        </div>
-        <div className="breadcrumb-end active customer" />
+      <div className="field select-country">
+        <div className="label">CONSIGNEE</div>
+        <input
+          value={shipment.consignee || ''}
+          onChange={e => dispatchers.onChangeConsignee(e.target.value)}
+        />
       </div>
+      <div className="field select-country">
+        <div className="label">NOTIFY PARTY</div>
+        <input
+          value={shipment.notifyParty || ''}
+          onChange={e => dispatchers.onChangeNotifyParty(e.target.value)}
+        />
+      </div>
+      <div className="field select-country">
+        <div className="label">TERMS OF SALE</div>
+        <Select
+          value={shipment.movement.termsOfSale}
+          options={APIGlobals.incotermOptions}
+          onChange={selectedValue =>
+            dispatchers.onChangeMovementTermsOfSale(selectedValue.value)}
+          disabled={shipment.movement.mode === 'Brokerage'}
+        />
+      </div>
+    </div>
+    <div className="pickup-delivery-wrapper">
+      <div className="cargo-row-icon" />
+      <div className="field select-country">
+        <textarea
+          className="address"
+          value={shipment.shipperAddress || ''}
+          onChange={e => dispatchers.onChangeShipperAddress(e.target.value)}
+        />
+      </div>
+      <div className="field select-country">
+        <textarea
+          className="address"
+          value={shipment.consigneeAddress || ''}
+          onChange={e => dispatchers.onChangeConsigneeAddress(e.target.value)}
+        />
+      </div>
+      <div className="field select-country">
+        <textarea
+          className="address"
+          value={shipment.notifyPartyAddress || ''}
+          onChange={e => dispatchers.onChangeNotifyPartyAddress(e.target.value)}
+        />
+      </div>
+    </div>
+    <div className="pickup-delivery-wrapper">
+      <div className="cargo-row-icon" />
+      <div className="field select-country">
+        <div className="label">CUSTOMER REFERENCE</div>
+        <input
+          value={shipment.customerReference || ''}
+          onChange={e => dispatchers.onChangeCustomerReference(e.target.value)}
+        />
+      </div>
+      <div className="field select-country">
+        <div className="label">B/L TYPE</div>
+        <Select
+          value={shipment.blType || ''}
+          options={[
+            { value: 'Waybill', label: 'Waybill' },
+            { value: 'Original', label: 'Original' },
+          ]}
+          onChange={opt => dispatchers.onChangeBLType(opt.value)}
+        />
+      </div>
+    </div>
+    <div className="title">
+      <div className="cargo-row-icon" />
+      CARGO
+    </div>
+    <EditCargo
+      cargo={shipment.cargo}
+      dispatchers={dispatchers}
+      useContainers={shipment.movement.mode === 'Sea'}
+      splitCargoTypes={false}
+      useDescription
+    />
+    <div className="title">
+      <div className="cargo-row-icon" />
+      MOVEMENT
+    </div>
+    <EditMovement
+      movement={shipment.movement}
+      dispatchers={dispatchers}
+      useDates
+      useShipperConsignee
+    />
+    <div className="form-button-group">
       <button
-        className="button-primary"
+        className="delete-button"
         onClick={() =>
-          history.push(`/customers/view/${match.params.customerId}/overview`)}
+          Shipment.archive(match.params.shipmentId, archivedShipment =>
+            dispatchers.loadShipment(archivedShipment),
+          )}
       >
-        BACK TO CUSTOMER
+        ARCHIVE
       </button>
+      <button
+        className="save-button"
+        onClick={() => Shipment.save(match.params.shipmentId, shipment)}
+      >
+        SAVE
+      </button>
+      {shipment.status === 'Unconfirmed'
+        ? <ConfirmBookingButton
+            shipment={shipment}
+            match={match}
+            dispatchers={dispatchers}
+          />
+        : null}
+      {shipment.status === 'Confirmed' && shipment.movement.mode === 'Sea'
+        ? <BillOfLadingButton shipment={shipment} />
+        : null}
+      {shipment.status === 'Confirmed' && shipment.movement.mode === 'Air'
+        ? <AirWaybillButton shipment={shipment} />
+        : null}
     </div>
-    <div className="panel container form-section">
+  </div>;
+
+const EditShipmentAccounting = ({ shipment, dispatchers }) => <div />;
+
+const EditShipment = ({ shipment, dispatchers, history, match, location }) =>
+  <div className="new-quote">
+    <div className="document-header">
       <div className="title">
-        <div className="cargo-row-icon" />
-        PARTIES
+        <span className="reference">
+          {shipment.reference}
+        </span>
+        <span className="backslash"> / </span>
+        <span className="page">
+          {location.pathname.indexOf('operations') !== -1
+            ? 'OPERATIONS'
+            : 'ACCOUNTING'}
+        </span>
       </div>
-      <div className="pickup-delivery-wrapper">
-        <div className="cargo-row-icon" />
-        <div className="field select-country">
-          <div className="label">SHIPPER</div>
-          <input
-            value={shipment.shipper || ''}
-            onChange={e => dispatchers.onChangeShipper(e.target.value)}
-          />
-        </div>
-        <div className="field select-country">
-          <div className="label">CONSIGNEE</div>
-          <input
-            value={shipment.consignee || ''}
-            onChange={e => dispatchers.onChangeConsignee(e.target.value)}
-          />
-        </div>
-        <div className="field select-country">
-          <div className="label">NOTIFY PARTY</div>
-          <input
-            value={shipment.notifyParty || ''}
-            onChange={e => dispatchers.onChangeNotifyParty(e.target.value)}
-          />
-        </div>
-        <div className="field select-country">
-          <div className="label">TERMS OF SALE</div>
-          <Select
-            value={shipment.movement.termsOfSale}
-            options={APIGlobals.incotermOptions}
-            onChange={selectedValue =>
-              dispatchers.onChangeMovementTermsOfSale(selectedValue.value)}
-            disabled={shipment.movement.mode === 'Brokerage'}
-          />
-        </div>
-      </div>
-      <div className="pickup-delivery-wrapper">
-        <div className="cargo-row-icon" />
-        <div className="field select-country">
-          <textarea
-            className="address"
-            value={shipment.shipperAddress || ''}
-            onChange={e => dispatchers.onChangeShipperAddress(e.target.value)}
-          />
-        </div>
-        <div className="field select-country">
-          <textarea
-            className="address"
-            value={shipment.consigneeAddress || ''}
-            onChange={e => dispatchers.onChangeConsigneeAddress(e.target.value)}
-          />
-        </div>
-        <div className="field select-country">
-          <textarea
-            className="address"
-            value={shipment.notifyPartyAddress || ''}
-            onChange={e =>
-              dispatchers.onChangeNotifyPartyAddress(e.target.value)}
-          />
-        </div>
-      </div>
-      <div className="pickup-delivery-wrapper">
-        <div className="cargo-row-icon" />
-        <div className="field select-country">
-          <div className="label">CUSTOMER REFERENCE</div>
-          <input
-            value={shipment.customerReference || ''}
-            onChange={e =>
-              dispatchers.onChangeCustomerReference(e.target.value)}
-          />
-        </div>
-        <div className="field select-country">
-          <div className="label">B/L TYPE</div>
-          <Select
-            value={shipment.blType || ''}
-            options={[
-              { value: 'Waybill', label: 'Waybill' },
-              { value: 'Original', label: 'Original' },
-            ]}
-            onChange={opt => dispatchers.onChangeBLType(opt.value)}
-          />
-        </div>
-      </div>
-      <div className="title">
-        <div className="cargo-row-icon" />
-        CARGO
-      </div>
-      <EditCargo
-        cargo={shipment.cargo}
-        dispatchers={dispatchers}
-        useContainers={shipment.movement.mode === 'Sea'}
-        splitCargoTypes={false}
-        useDescription
-      />
-      <div className="title">
-        <div className="cargo-row-icon" />
-        MOVEMENT
-      </div>
-      <EditMovement
-        movement={shipment.movement}
-        dispatchers={dispatchers}
-        useDates
-        useShipperConsignee
-      />
-      <div className="form-button-group">
-        <button
-          className="delete-button"
-          onClick={() =>
-            Shipment.archive(match.params.shipmentId, archivedShipment =>
-              dispatchers.loadShipment(archivedShipment),
-            )}
+      <div className="tabs">
+        <Link
+          to={`/customers/view/${match.params.customerId}/shipments/${match
+            .params.shipmentId}/operations`}
         >
-          ARCHIVE
-        </button>
-        <button
-          className="save-button"
-          onClick={() => Shipment.save(match.params.shipmentId, shipment)}
+          operations
+        </Link>
+        <span className="backslash"> / </span>
+        <Link
+          to={`/customers/view/${match.params.customerId}/shipments/${match
+            .params.shipmentId}/accounting`}
         >
-          SAVE
-        </button>
-        {shipment.status === 'Unconfirmed'
-          ? <ConfirmBookingButton
-              shipment={shipment}
-              match={match}
-              dispatchers={dispatchers}
-            />
-          : null}
-        {shipment.status === 'Confirmed' && shipment.movement.mode === 'Sea'
-          ? <BillOfLadingButton shipment={shipment} />
-          : null}
-        {shipment.status === 'Confirmed' && shipment.movement.mode === 'Air'
-          ? <AirWaybillButton shipment={shipment} />
-          : null}
+          accounting
+        </Link>
       </div>
     </div>
+    <Route
+      path="/customers/view/:customerId/shipments/:shipmentId/operations"
+      render={props =>
+        <EditShipmentOperations
+          shipment={shipment}
+          dispatchers={dispatchers}
+          match={match}
+          {...props}
+        />}
+    />
+    <Route
+      path="/customers/view/:customerId/shipments/:shipmentId/accounting"
+      render={props =>
+        <EditShipmentAccounting
+          shipment={shipment}
+          dispatchers={dispatchers}
+        />}
+    />
   </div>;
 
 EditShipment.propTypes = {
