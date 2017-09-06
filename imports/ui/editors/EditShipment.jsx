c1d9df3131773c12e320346fbfd181c722943677
James Burgess <jamesmillerburgess@gmail.com>
2017-09-06 19:40:34 +0200

    Most of the accounting layout

--- imports/ui/editors/EditShipment.jsx
+++ imports/ui/editors/EditShipment.jsx
@@ -190,7 +190,107 @@ export const EditShipmentOperations = ({ shipment, dispatchers, match }) =>
     </div>
   </div>;
 
-const EditShipmentAccounting = ({ shipment, dispatchers }) => <div />;
+const EditShipmentAccounting = ({ shipment, dispatchers }) =>
+  <div className="panel container form-section">
+    <div className="invoicing-buttons">
+      <div className="invoice-button-group">
+        <div className="title">Sales Invoices</div>
+        <button className="button-primary">
+          <span className="fa fa-fw fa-check-circle" />
+          Pyrotek Engineering Materials Ltd
+        </button>
+        <button className="button-disabled">
+          <span className="fa fa-fw fa-ban" />ABC Widgets
+        </button>
+      </div>
+      <div className="invoice-button-group">
+        <div className="title">Purchase Invoices</div>
+        <button className="button-primary">
+          <span className="fa fa-fw fa-ellipsis-h" />
+          British Airways
+        </button>
+        <button className="button-primary">
+          <span className="fa fa-fw fa-ellipsis-h" />ABC Truckers
+        </button>
+      </div>
+    </div>
+    <div className="charges-editor">
+      <div className="header-row">
+        <div className="revenue-side">
+          <button className="cargo-row-icon">
+            <span className="fa fa-fw fa-plus-square" />
+          </button>
+          <div className="title charge-name">External Charges</div>
+          <div className="title">Revenue</div>
+        </div>
+        <div className="cost-side">
+          <div className="title">Cost</div>
+        </div>
+      </div>
+      <div className="header-row">
+        <div className="revenue-side">
+          <button className="cargo-row-icon">
+            <span className="fa fa-fw fa-minus-square" />
+          </button>
+          <input className="charge-name" value="International Freight" />
+          <input className="charge-name" value="Pyrotek Engineering Materials Ltd" />
+          <input className="" value="200" />
+          <input className="currency-field" value="USD" />
+        </div>
+        <div className="cost-side">
+          <input className="charge-name" value="British Airways" />
+          <input className="" value="150" />
+          <input className="currency-field" value="USD" />
+        </div>
+      </div>
+      <div className="header-row">
+        <div className="revenue-side">
+          <button className="cargo-row-icon">
+            <span className="fa fa-fw fa-minus-square" />
+          </button>
+          <input className="charge-name" value="Destination Transport" />
+          <input className="charge-name" value="Pyrotek Engineering Materials Ltd" />
+          <input className="" value="200" />
+          <input className="currency-field" value="USD" />
+        </div>
+        <div className="cost-side">
+          <input className="charge-name" value="ABC Truckers" />
+          <input className="" value="150" />
+          <input className="currency-field" value="USD" />
+        </div>
+      </div>
+    </div>
+    <div className="charges-editor">
+      <div className="header-row">
+        <div className="revenue-side">
+          <button className="cargo-row-icon">
+            <span className="fa fa-fw fa-plus-square" />
+          </button>
+          <div className="title charge-name">Internal Charges</div>
+          <div className="title">Bill To</div>
+        </div>
+        <div className="cost-side">
+          <div className="title">Bill From</div>
+        </div>
+      </div>
+      <div className="header-row">
+        <div className="revenue-side">
+          <button className="cargo-row-icon">
+            <span className="fa fa-fw fa-minus-square" />
+          </button>
+          <input className="charge-name" value="Destination Transport" />
+          <input className="charge-name" value="Basel" />
+          <input className="" value="200" />
+          <input className="currency-field" value="USD" />
+        </div>
+        <div className="cost-side">
+          <input className="charge-name" value="Miami" />
+          <input className="" value="200" />
+          <input className="currency-field" value="USD" />
+        </div>
+      </div>
+    </div>
+  </div>;
 
 const EditShipment = ({ shipment, dispatchers, history, match, location }) =>
   <div className="new-quote">
