ca55f333d5639eca54029f8e860f3d5a586baef3
James Burgess <jamesmillerburgess@gmail.com>
2017-09-16 08:28:48 +0200

    SupplierListItem

--- imports/ui/list-items/SupplierListItem.jsx
+++ imports/ui/list-items/SupplierListItem.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SupplierListItem = () => <div>supplier</div>;
+const SupplierListItem = ({ supplier }) =>
+  <Link
+    to={`/suppliers/view/${supplier._id}/overview`}
+    className="list-item panel"
+  >
+    <div className="title">
+      {supplier.name}
+    </div>
+  </Link>;
 
 export default SupplierListItem;
