1cfadccc0f433f1c8f757237dfab16451a986624
James Burgess <jamesmillerburgess@gmail.com>
2017-09-16 08:02:33 +0200

    Supplier list route and component stub

--- imports/ui/app-core/Main.jsx
+++ imports/ui/app-core/Main.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import Nav from '../../ui/app-core/Nav.jsx';
 import Home from '../pages/Home.jsx';
 import CustomerListConnect from '../lists/CustomerListConnect.jsx';
+import SupplierList from '../lists/SupplierList.jsx';
 import EditCustomerConnect from '../editors/EditCustomerConnect';
 import CustomerConnect from '../pages/CustomerConnect';
 import RateList from '../lists/RateList.jsx';
@@ -36,103 +37,109 @@ const MainInner = ({ loading }) => {
   return (
     <BrowserRouter>
       <Route
-        render={props => (
+        render={props =>
           <div className="app-inner">
             <Nav {...props} />
-            {loading ?
-             '' :
-             <div className="content-container">
-               <Route
-                 path="/"
-                 exact
-                 render={routeProps => <Home {...routeProps} />}
-               />
-               <Route
-                 path="/sign-up"
-                 render={routeProps => <SignUp {...routeProps} />}
-               />
-               <Route
-                 path="/sign-in"
-                 render={routeProps => <SignIn {...routeProps} />}
-               />
-               <Route
-                 path="/customers"
-                 exact
-                 render={routeProps => verifyAuth(
-                   CustomerListConnect,
-                   routeProps,
-                 )}
-               />
-               <Route
-                 path="/customers/new"
-                 exact
-                 render={routeProps => verifyAuth(
-                   EditCustomerConnect,
-                   routeProps,
-                 )}
-               />
-               <Route
-                 path="/customers/view/:customerId"
-                 render={routeProps => verifyAuth(CustomerConnect, routeProps)}
-               />
-               <Route
-                 path="/customers/edit/:customerId"
-                 render={routeProps => verifyAuth(EditCustomerConnect, {
-                   ...routeProps,
-                   editMode: true,
-                 })}
-               />
-               <Route
-                 path="/rates"
-                 exact
-                 render={routeProps => verifyAdminAuth(RateList, {
-                   ...routeProps,
-                 })}
-               />
-               <Route
-                 path="/rates/new"
-                 render={routeProps => verifyAdminAuth(EditRateConnect, {
-                   ...routeProps,
-                 })}
-               />
-               <Route
-                 path="/rates/edit/:rateId"
-                 render={routeProps => verifyAdminAuth(EditRateConnect, {
-                   ...routeProps,
-                   editMode: true,
-                 })}
-               />
-               <Route
-                 path="/profile"
-                 render={routeProps => verifyAuth(UserProfileConnect, {
-                   ...routeProps,
-                   editMode: true,
-                 })}
-               />
-               <Route
-                 path="/branches"
-                 exact
-                 render={routeProps => verifyAdminAuth(BranchList, routeProps)}
-               />
-               <Route
-                 path="/branches/new"
-                 exact
-                 render={routeProps => verifyAdminAuth(
-                   EditBranchConnect,
-                   routeProps,
-                 )}
-               />
-               <Route
-                 path="/branches/edit/:branchId"
-                 render={routeProps => verifyAdminAuth(EditBranchConnect, {
-                   ...routeProps,
-                   editMode: true,
-                 })}
-               />
-             </div>
-            }
-          </div>
-        )}
+            {loading
+              ? ''
+              : <div className="content-container">
+                  <Route
+                    path="/"
+                    exact
+                    render={routeProps => <Home {...routeProps} />}
+                  />
+                  <Route
+                    path="/sign-up"
+                    render={routeProps => <SignUp {...routeProps} />}
+                  />
+                  <Route
+                    path="/sign-in"
+                    render={routeProps => <SignIn {...routeProps} />}
+                  />
+                  <Route
+                    path="/customers"
+                    exact
+                    render={routeProps =>
+                      verifyAuth(CustomerListConnect, routeProps)}
+                  />
+                  <Route
+                    path="/customers/new"
+                    exact
+                    render={routeProps =>
+                      verifyAuth(EditCustomerConnect, routeProps)}
+                  />
+                  <Route
+                    path="/customers/view/:customerId"
+                    render={routeProps =>
+                      verifyAuth(CustomerConnect, routeProps)}
+                  />
+                  <Route
+                    path="/customers/edit/:customerId"
+                    render={routeProps =>
+                      verifyAuth(EditCustomerConnect, {
+                        ...routeProps,
+                        editMode: true,
+                      })}
+                  />
+                  <Route
+                    path="/suppliers"
+                    exact
+                    render={routeProps =>
+                      verifyAuth(SupplierList, { ...routeProps })}
+                  />
+                  <Route
+                    path="/rates"
+                    exact
+                    render={routeProps =>
+                      verifyAdminAuth(RateList, {
+                        ...routeProps,
+                      })}
+                  />
+                  <Route
+                    path="/rates/new"
+                    render={routeProps =>
+                      verifyAdminAuth(EditRateConnect, {
+                        ...routeProps,
+                      })}
+                  />
+                  <Route
+                    path="/rates/edit/:rateId"
+                    render={routeProps =>
+                      verifyAdminAuth(EditRateConnect, {
+                        ...routeProps,
+                        editMode: true,
+                      })}
+                  />
+                  <Route
+                    path="/profile"
+                    render={routeProps =>
+                      verifyAuth(UserProfileConnect, {
+                        ...routeProps,
+                        editMode: true,
+                      })}
+                  />
+                  <Route
+                    path="/branches"
+                    exact
+                    render={routeProps =>
+                      verifyAdminAuth(BranchList, routeProps)}
+                  />
+                  <Route
+                    path="/branches/new"
+                    exact
+                    render={routeProps =>
+                      verifyAdminAuth(EditBranchConnect, routeProps)}
+                  />
+                  <Route
+                    path="/branches/edit/:branchId"
+                    render={routeProps =>
+                      verifyAdminAuth(EditBranchConnect, {
+                        ...routeProps,
+                        editMode: true,
+                      })}
+                  />
+                </div>}
+          </div>}
       />
     </BrowserRouter>
   );
@@ -145,11 +152,7 @@ MainInner.propTypes = {
 const Main = createContainer(() => {
   const branches = Meteor.subscribe('branch.all');
   let branch;
-  if (
-    Meteor.user() &&
-    Meteor.user().profile &&
-    Meteor.user().profile.branch
-  ) {
+  if (Meteor.user() && Meteor.user().profile && Meteor.user().profile.branch) {
     branch = Meteor.subscribe('branch.active', Meteor.user().profile.branch);
   } else {
     branch = Meteor.subscribe('branch.active');
---
+++ imports/ui/lists/SupplierList.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SupplierList = (props) => <div />;
+
+export default SupplierList;
