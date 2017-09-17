fab5e260009f8de526e43631983c69bd1ae12dac
James Burgess <jamesmillerburgess@gmail.com>
2017-09-17 17:14:47 +0200

    Cleanup

--- imports/ui/editors/EditShipment.jsx
+++ imports/ui/editors/EditShipment.jsx
@@ -17,9 +17,13 @@ const EditShipment = ({
   <div className="new-quote">
     <div className="document-header">
       <div className="title">
-        <span className="reference">{shipment.reference}</span>
+        <span className="reference">
+          {shipment.reference}
+        </span>
         <span className="backslash"> / </span>
-        <span className="page">{activeTab}</span>
+        <span className="page">
+          {activeTab}
+        </span>
       </div>
       <div className="tabs">
         <Link to={toOperations}>operations</Link>
@@ -40,6 +44,14 @@ const EditShipment = ({
 EditShipment.propTypes = {
   shipment: PropTypes.object.isRequired,
   dispatchers: PropTypes.objectOf(PropTypes.func).isRequired,
+};
+
+EditShipment.defaultProps = {
+  activeTab: '',
+  toOperations: '',
+  toAccounting: '',
+  shipmentOperationsPath: '',
+  shipmentAccountingPath: '',
 };
 
 export default EditShipment;
